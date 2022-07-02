feba5100ec653b5968ce2b949fab82ba2791830a
anomander96 <mister.truepaz@gmail.com>
2022-07-02 18:19:44 +0300

    Initial commit

---
+++ src/main/java/com/anomander/noSql/NoSqlApplication.java
@@ -0,0 +1,13 @@
+package com.anomander.noSql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anomander/noSql/NoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anomander.noSql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
